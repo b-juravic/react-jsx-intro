2519148dacf7d1dfc48c37e0825081b23737ed93
Brittiany Juravic <britjuravic@Brittianys-MacBook-Pro.local>
2020-04-20 12:43:41 -0700

    Part 1 Updated- Now complete

--- part-1/FirstComponent/index.js
+++
@@ -1,2 +0,0 @@
-ReactDOM.render(<FirstComponent/>,
-  document.getElementById("root"));
--- part-1/index.js
+++ part-1/index.js
@@ -1,4 +1,4 @@
 ReactDOM.render(
-  <NamedComponent name="Brittiany" />,
+  <App />,
   document.getElementById("root")
 );
